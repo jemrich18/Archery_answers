4828c749549a8fcf6ee5be46d94ad050e5a05d69
jemrich18 <jemrich18@gmail.com>
2026-04-03 15:00:07 -0500

    Fix static files storage for Django 6

--- core/settings.py
+++ core/settings.py
@@ -93,7 +93,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Auth
 LOGIN_URL = '/accounts/login/'
